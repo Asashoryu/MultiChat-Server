73cefa61f4d4c6a5667dfba19e5956f3308763e7
trincalex <al.trincone@studenti.unina.it>
2023-01-23 17:51:23 +0100

    cioa

---
+++ provaMain.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "gestore_pacchetti.h"
+
+int main ()
+{
+    char pac[200];
+    strcpy (pac,"cmd=100\r\nnome=Alex\r\npassword=pass");
+    printf ("%s\n",pac);
+    char *cmd = parse_comando (pac);
+    printf ("%s\n",cmd);
+    return 0;
+}
