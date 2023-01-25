110b13f04dee068889406de4a577a21b88d31d1e
trincalex <al.trincone@studenti.unina.it>
2023-01-25 13:07:14 +0100

    ciao

--- provaMain.c
+++ provaMain.c
@@ -5,8 +5,8 @@
 #include "gestore_pacchetti.h"
 #include <stdio.h>
 #include <string.h>
-//#include <libpq-fe.h>
-//#include "database.h"
+#include <libpq-fe.h>
+#include "database.h"
 
 //gcc -o prova provaMain.c database.c -I/usr/include/postgresql -L/usr/lib/postgresql/15/lib -lpq
 
@@ -20,15 +20,24 @@ int main ()
     char amministratore[200];
     char *password;
     char user[200];
+    char gruppo[200];
+    char mess[200];
     int num,i;
-    //PGresult *risultato;
+    PGresult *risultato;
 
-    strcpy(pk,"cmd=100\r\nnome=Pino\r\npassword=pass\r\n\r\n");
-    alloca_signin (&nome,&password);
-    parse_signin (pk,nome,password);
-    printf ("%s %s\n",nome,password);
-    dealloca_signin(&nome,&password);
+    // strcpy(pk,"cmd=100\r\nnome=Pino\r\npassword=pass\r\n\r\n");
+    // alloca_signin (&nome,&password);
+    // parse_signin (pk,nome,password);
+    // printf ("%s %s\n",nome,password);
+    // dealloca_signin(&nome,&password);
 
+    strcpy (gruppo,"Gruppo1");
+    strcpy (user,"Pino");
+    strcpy (mess,"Welaaaaa");
+
+    flag = insert_notifica_db (user,gruppo);
+    if (flag == 1)
+        printf ("OK\n");
     // risultato = select_gruppi_senza_utente (user);
     // num = PQntuples (risultato);
     // for (i=0;i<num;i++)
